161b01e7f8d9063026a4c57461a7e46528303a97
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-02-22 15:10:34 -0500

    Update undertone.py

--- computation/undertone.py
+++ computation/undertone.py
@@ -1,1 +1,10 @@
-
+def findfactorlin(iteration, slope, intercept):
+	return
+def findfactorpoly():
+	return
+def findfactorinvpoly():
+	return
+def findfactorrat():
+	return
+def findfactorsin():
+	return
